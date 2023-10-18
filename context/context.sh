f782dfc6c0935d66fd58cbc70ac328e982e9c47f
abhay-precize <120437863+abhay-precize@users.noreply.github.com>
2023-10-18 14:23:19 +0530

    Create context.sh

---
+++ context/context.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export YOR_VERSION=0.1.183
+wget -q -O - https://github.com/bridgecrewio/yor/releases/download/${YOR_VERSION}/yor_${YOR_VERSION}_linux_amd64.tar.gz | tar -xvz -C /tmp
+changedDirs=$(git log -m -1 --name-only --pretty="format:" ${BITBUCKET_COMMIT} | sed '/^[[:space:]]*$/d' | xargs dirname | sort -u)
+dirArr=($changedDirs)
+
+for dir in "${dirArr[@]}"
+do 
+    /tmp/yor tag -d $dir --tag-groups git --skip-tags git_org,git_modifiers,git_last_modified_by,git_last_modified_at --parsers Terraform --tag-prefix precize_ --tag-local-modules false
+done
+
+lines=$(git status -s -uno | wc -l)
+if [ $lines -gt 0 ];then
+    git config --global user.name "Precize"
+    git config --global user.email "noreply@precize.ai"
+    git commit -am "[skip ci] Updating changes.txt with latest build number."
+    git push
+fi
