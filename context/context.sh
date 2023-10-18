51aa93ddccdc7fcdac7f483a2841217f2bd3e8fe
abhayanoop <abhay@precize.io>
2023-10-18 14:47:37 +0530

    Updated commit message; Ignoring pipeline file changes

--- context/context.sh
+++ context/context.sh
@@ -2,11 +2,11 @@
 
 export YOR_VERSION=0.1.183
 wget -q -O - https://github.com/bridgecrewio/yor/releases/download/${YOR_VERSION}/yor_${YOR_VERSION}_linux_amd64.tar.gz | tar -xvz -C /tmp
-changedDirs=$(git log -m -1 --name-only --pretty="format:" ${BITBUCKET_COMMIT} | sed '/^[[:space:]]*$/d' | xargs dirname | sort -u)
+changedDirs=$(git log -m -1 --name-only --pretty="format:" ${BITBUCKET_COMMIT} | grep -v '.bitbucket-pipelines.yml' | sed '/^[[:space:]]*$/d' | xargs dirname | sort -u)
 dirArr=($changedDirs)
 
 for dir in "${dirArr[@]}"
-do 
+do
     /tmp/yor tag -d $dir --tag-groups git --skip-tags git_org,git_modifiers,git_last_modified_by,git_last_modified_at --parsers Terraform --tag-prefix precize_ --tag-local-modules false
 done
 
@@ -14,6 +14,6 @@ lines=$(git status -s -uno | wc -l)
 if [ $lines -gt 0 ];then
     git config --global user.name "Precize"
     git config --global user.email "noreply@precize.ai"
-    git commit -am "[skip ci] Updating changes.txt with latest build number."
+    git commit -am "Resource tags updated for Terraform configurations by Precize"
     git push
 fi
