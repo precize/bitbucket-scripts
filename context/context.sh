be743862e21f6f9ff51bdab8359448c1b0d8f863
abhayanoop <abhay@precize.io>
2023-11-11 00:48:58 +0530

    Precize maintained binary for context

--- context/context.sh
+++ context/context.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-export YOR_VERSION=0.1.183
-wget -q -O - https://github.com/bridgecrewio/yor/releases/download/${YOR_VERSION}/yor_${YOR_VERSION}_linux_amd64.tar.gz | tar -xvz -C /tmp
+wget -q -O - https://precize-terraform-context.s3.amazonaws.com/precize-context.tgz | tar -xvz -C /tmp
 changedDirs=$(git log -m -1 --name-only --pretty="format:" ${BITBUCKET_COMMIT} | grep -vw 'bitbucket-pipelines.yml' | sed '/^[[:space:]]*$/d' | xargs dirname | sort -u)
 dirArr=($changedDirs)
 
 for dir in "${dirArr[@]}"
 do
-    /tmp/yor tag -d $dir --tag-groups git --skip-tags git_org,git_modifiers,git_last_modified_by,git_last_modified_at --parsers Terraform --tag-local-modules false
+    /tmp/precize-context tag -d $dir --tag-groups git --skip-tags git_org,git_modifiers,git_last_modified_by,git_last_modified_at --parsers Terraform --tag-prefix precize_ --tag-local-modules false
 done
 
 lines=$(git status -s -uno | wc -l)
